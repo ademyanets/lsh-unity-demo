19cf51f7509ff9eeaf45b7ea177ddd95e27e4e83
Andrey Demyanets <am.demianetc@severgroup.ru>
2019-05-21 10:41:49 +0300

    Simple movement with walk animations

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public Joystick joystick;
+    public float speed;
+    public Animator animator;
+
+
+    Vector2 move;
+    bool flip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = joystick.Direction * speed; //Должно быть уже нормализовано джойстиком
+
+        animator.SetBool("Walk", System.Math.Abs(joystick.Horizontal) > 0.0f);
+
+        if (joystick.Horizontal > 0 && flip || joystick.Horizontal < 0.0f && !flip)
+        {
+            flip = !flip;
+            Vector2 tmp = transform.localScale;
+            tmp.x *= -1;
+            transform.localScale = tmp;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move * Time.deltaTime);
+
+    }
+}
