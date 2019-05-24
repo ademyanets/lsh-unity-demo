9e449a0db11b06e300d59602a2271938e5d0e7b9
Andrey Demyanets <am.demianetc@severgroup.ru>
2019-05-24 09:52:07 +0300

    add coins [wip]

---
+++ Assets/Scripts/CoinObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinObject : MonoBehaviour
+{
+    public int coinValue = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ScoreManager.instance.increase(coinValue);
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public static ScoreManager instance;
+    public TextMeshProUGUI text;
+    int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = new ScoreManager();
+        }
+    }
+
+    public void increase(int value)
+    {
+        score += value;
+        text.text = "X" + score.ToString();
+    }
+}
