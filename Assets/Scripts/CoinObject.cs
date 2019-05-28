68bacab289caee1ed70b8400eca2cc4af7057ec6
Andrey Demyanets <am.demianetc@severgroup.ru>
2019-05-28 06:44:55 +0300

    add coin physics

--- Assets/Scripts/CoinObject.cs
+++ Assets/Scripts/CoinObject.cs
@@ -7,22 +7,6 @@ public class CoinObject : MonoBehaviour
     public int coinValue = 1;
     public float fallSpeed = 5;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    
-    }
-
-    private void Update()
-    {
-        transform.position -= new Vector3(0, fallSpeed * Time.deltaTime, 0);
-    }
-
-    private void FixedUpdate()
-    {
-        //transform.position
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
